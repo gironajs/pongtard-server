f8fe0ccd47b32a4113139a273b6d5da371540868
Josep Subils <warlock@users.noreply.github.com>
2017-10-25 11:50:24 +0200

    [up] Change web config

--- public/index.js
+++ public/index.js
@@ -1,4 +1,4 @@
-const socket = io('http://192.168.0.158:3010')
+const socket = io(`http://${window.location.hostname}:3010`)
 
 document.addEventListener("DOMContentLoaded", () => {
   const up = document.getElementById('up')
